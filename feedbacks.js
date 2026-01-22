4858746744c79ebf94073f5668be86f067824213
hphuong412 <hphuong412205@gmail.com>
2026-01-22 15:26:25 +0700

    push

--- feedbacks.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const feedbackController = require('../controllers/feedbackController');
-const { protect } = require('../middleware/auth');  // ✅ đúng
-
-router.get('/course/:courseId', feedbackController.getFeedbacksByCourse);
-router.post('/course/:courseId', protect, feedbackController.addFeedback); // ✅ protect là function
-
-module.exports = router;
--- hash.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-(async () => {
-  console.log("admin123:", await bcrypt.hash("admin123", 10));
-  console.log("user123 :", await bcrypt.hash("user123", 10));
-})();
